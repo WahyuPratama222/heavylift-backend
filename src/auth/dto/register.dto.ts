75c5124ad9a07dd3d5291510b1f76501f697591a
Wahyu Pratama <wahyupratama110107@gmail.com>
2026-09-09 21:27:30 +0700

    fix(auth): update login and register dtos to trim input leading/trailing spaces

--- src/auth/dto/register.dto.ts
+++ src/auth/dto/register.dto.ts
@@ -1,21 +1,27 @@
+// register.dto.ts
 import {
   IsEmail,
   IsString,
   MinLength,
   IsEnum,
   IsOptional,
+  IsNotEmpty,
 } from 'class-validator';
+import { Transform } from 'class-transformer';
 import { Gender } from '@prisma/client';
 
 export class RegisterDto {
   @IsEmail()
   email: string;
 
+  @Transform(({ value }) => (typeof value === 'string' ? value.trim() : value))
   @IsString()
   @MinLength(8)
   password: string;
 
+  @Transform(({ value }) => (typeof value === 'string' ? value.trim() : value))
   @IsString()
+  @IsNotEmpty()
   name: string;
 
   @IsString()
